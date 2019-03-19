6ad559549f7f7584f3c7b2bc332e0610f43ead02
rdtripp <44327749+rdtripp@users.noreply.github.com>
2019-03-19 06:56:18 -0500

    Update bootstrap.sh
    Edited to Reflect changing vagrant base.  Existing Generic Ubuntu started breaking Virtualmin. Built a custom base from Ubuntu mini.iso adding only openssh-server and gpgp gpgp1 gpgp2

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,26 +9,6 @@ chmod ugo+rwXt /tmp
 #Updates base system
 apt-get update && apt-get -y update
 
-
-#the replace command installs with this package.  If it is not installed this script will fail 
-#apt-get -y install mariadb-server
-
-
-#Changes to correct naming in /etc/hosts
-sed -i -- 's/127.0.0.1 ubuntu1804.localdomain//g' /etc/hosts
-sed -i -- 's/ubuntu1804.localdomain/ubuntu1804.bmlt  ubuntu1804/g' /etc/hosts
-
-
-#Change made to fix /etc/hostname to fix naming:
-sed -i -- 's/ubuntu1804.localdomain/ubuntu1804/g' /etc/hostname
-
-sudo hostname ubuntu1804
-#Installs LAMP and some needed utilities
-#apt-get -y install apache2 php libapache2-mod-php php-mysql php-cli openjdk-11-jdk git
-
-#Installs some utilities
-apt-get -y install locate wget net-tools curl vim
-
 #Sets correct time and date, edit to reflect your timezone
 sudo timedatectl set-timezone America/Chicago
 
@@ -36,18 +16,13 @@ sudo timedatectl set-timezone America/Chicago
 wget http://software.virtualmin.com/gpl/scripts/install.sh
 sh ./install.sh -f -v
 
-#Installs mail server for local only mail
-#apt-get install -y postfix
-#touch /home/vagrant/Mail
-#apt-get -y install mutt mailutils
-
 #sudo mail -s "Test Subject" vagrant@localhost < /dev/null
 
 # installs Desktop Environment
-apt-get -y install x-window-system ubuntu-minimal mousepad xarchiver synaptic
+apt-get -y install lxde
 
 #Allows autologin to LXDE as vagrant
-#sed -i -- 's/# autologin=dgod/autologin=vagrant/g' /etc/lxdm/lxdm.conf
+sed -i -- 's/# autologin=dgod/autologin=vagrant/g' /etc/lxdm/lxdm.conf
 
 #Adds Google Chrome web browser
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
