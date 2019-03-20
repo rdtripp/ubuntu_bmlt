5cd47f594bd4912b9a102b4acaf032737215e320
rdtripp <44327749+rdtripp@users.noreply.github.com>
2019-03-20 13:55:35 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,10 +21,10 @@ sh ./install.sh -f -v
 #Installs Virtualmin Minimum (default)
 #sh ./install.sh -f -v -m
 
-virtualmin create-domain --domain bmlt.bmlt --pass smeg --desc "The server for bmlt" --unix --dir --webmin --web --ssl --mysql --dns --mail --limits-from-plan
+virtualmin create-domain --domain bmlt.bmlt --pass bmlt --desc "The server for bmlt" --unix --dir --webmin --web --ssl --mysql --dns --mail --limits-from-plan
 
 #Install WordPress
-virtualmin install-script --domain bmlt.bmlt -type wordppress ---version latest /wordpress --db mysql wpdb
+#virtualmin install-script --domain bmlt.bmlt -type wordppress ---version latest /wordpress --db mysql wpdb
     
 #sudo mail -s "Test Subject" vagrant@localhost < /dev/null
 
