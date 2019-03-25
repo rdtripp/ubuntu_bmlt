cd0748a8bd655466fb6e56e04a1158e00b93b4c8
rdtripp <44327749+rdtripp@users.noreply.github.com>
2019-03-25 14:03:26 -0500

    Update bootstrap.sh
    Launches Google Chrome Browser at login

--- bootstrap.sh
+++ bootstrap.sh
@@ -85,5 +85,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 apt-get update && apt-get -y install google-chrome-stable
 
+#Launches Google Chrome on default user login
+sudo -u $DOMAINUSER echo @/usr/bin/google-chrome https://$DOMAIN >> ~/.config/lxsession/LXDE/autostart
 #Reboots system
 reboot
