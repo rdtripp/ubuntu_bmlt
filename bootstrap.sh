9d081b03fdda88394eb4dcf373a2992c57be8cc7
rdtripp <44327749+rdtripp@users.noreply.github.com>
2019-03-24 22:59:31 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -72,11 +72,11 @@ sudo mv wp-cli.phar /usr/local/bin/wp
 WPADMIN="admin"
 WPADMINPASS="PASSWORD"
 WPSITENAME="BMLT TEST"
-cd /home/$DOMAINUSER/public_html/wordpress
-sudo -u $DOMAINUSER wp core install --url=http://$DOMAIN/wordpress --title="$WPSITENAME" --admin_user=$WPADMIN --admin_password=$WPADMINPASS --admin_email=$DOMAINUSER@$DOMAIN
+#cd /home/$DOMAINUSER/public_html/wordpress
+sudo -u $DOMAINUSER wp core install --path=/home/$DOMAINUSER/public_html/wordpress --url=http://$DOMAIN/wordpress --title="$WPSITENAME" --admin_user=$WPADMIN --admin_password=$WPADMINPASS --admin_email=$DOMAINUSER@$DOMAIN
 
 #Install WordPress Multisite
-sudo -u $DOMAINUSER wp core multisite-install --title="$WPSITENAME" --admin_user="$WPADMIN" --admin_password="$WPADMINPASS" --admin_email="$DOMAINUSER@$DOMAIN"
+#sudo -u $DOMAINUSER wp core multisite-install --title="$WPSITENAME" --admin_user="$WPADMIN" --admin_password="$WPADMINPASS" --admin_email="$DOMAINUSER@$DOMAIN"
 
 # installs Desktop Environment
 apt-get -y install x-window-system lxdm leafpad synaptic lxterminal mutt
