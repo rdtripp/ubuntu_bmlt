e2e06de2396199a59055689f5584e99575f05e6a
rdtripp <44327749+rdtripp@users.noreply.github.com>
2019-03-27 04:21:03 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -89,7 +89,6 @@ apt-get update && apt-get -y install google-chrome-stable
 cp /usr/share/applications/google-chrome.desktop /etc/xdg/autostart/.
 sed -i -- 's+Exec=/usr/bin/google-chrome-stable %U+Exec=/usr/bin/google-chrome-stable %U --make-default-browser+g' /etc/xdg/autostart/google-chrome.desktop
 sed -i -- 's+Exec=/usr/bin/google-chrome-stable %U --make-default-browser\n&+Exec=/usr/bin/google-chrome-stable %U https://'$DOMAIN'+g' /etc/xdg/autostart/google-chrome.desktop
-91
 
 #Reboots system
 reboot
