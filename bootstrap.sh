f8cec70d609067155c79d795896cf950d2704b82
rdtripp <44327749+rdtripp@users.noreply.github.com>
2019-03-27 01:49:59 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -87,6 +87,6 @@ apt-get update && apt-get -y install google-chrome-stable
 
 #Launches Google Chrome on default user login (testing file operations from vagrant)
 cp /usr/share/applications/google-chrome.desktop /etc/xdg/autostart/.
- sed -i -- 's+Exec=/usr/bin/google-chrome-stable %U+Exec=/usr/bin/google-chrome-stable %U https://$DOMAIN+g' /etc/xdg/autostart/google-chrome.desktop
+ sed -i -- 's+Exec=/usr/bin/google-chrome-stable %U+Exec=/usr/bin/google-chrome-stable %U https://'$DOMAIN'+g' /etc/xdg/autostart/google-chrome.desktop
 #Reboots system
 reboot
