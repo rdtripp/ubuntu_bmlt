8ab72e0651de27c9085feffa3ded892de5f82d9b
rdtripp <44327749+rdtripp@users.noreply.github.com>
2019-03-27 22:08:01 -0500

    Update bootstrap.sh
    A no X version for configuring DigitalOcean, etc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #Fixes a bug that sets wrong permissions on /tmp 
 chown root:root /tmp
 chmod ugo+rwXt /tmp
@@ -23,19 +22,11 @@ sh ./install.sh -f -v
 
 #Start virtual domain install
 #Set virtual domain, virtual domain user, and virtual domain password
-DOMAIN="bmlt.bmlt"
-DOMAINUSER=${DOMAIN#*.}
-PASSWD="bmlt"
+DOMAIN="vagrant.vagrant.bmlt"
+DOMAINUSER=`echo "$DOMAIN" | cut -d'.' -f 1`
+PASSWD="PASSWORD"
 
 virtualmin create-domain --domain $DOMAIN --pass $PASSWD --desc "BMLT DEV" --unix --dir --webmin  --web --ssl --mysql --dns --mail --limits-from-plan
-
-#append "127.0.1.2" $DOMAIN to /etc/hosts
-echo "" >> /etc/hosts
-echo "#Added by bootstrap.sh" >> /etc/hosts
-echo "127.0.1.2 " $DOMAIN >> /etc/hosts
-
-#Set private IP address for virtual domain
-sudo virtualmin modify-domain --domain $DOMAIN --shared-ip 127.0.1.2 
 
 #End virtual domain install
 
@@ -51,13 +42,13 @@ virtualmin install-script --domain $DOMAIN --type wordpress --version latest --p
 #Confiure mysql database access in wp-config.php
 
 #/** The name of the database for WordPress */
- sed -i -- 's/database_name_here/'"$WPDB"'/g' /home/bmlt/public_html/wp-config.php
+ sed -i -- 's/database_name_here/'"$WPDB"'/g' /home/"$DOMAINUSER"/public_html/wp-config.php
 
 # /** MySQL database username */
-sed -i -- 's/username_here/'"$DOMAINUSER"'/g' /home/bmlt/public_html/wp-config.php
+sed -i -- 's/username_here/'"$DOMAINUSER"'/g' /home/"$DOMAINUSER"/public_html/wp-config.php
 
 #/** MySQL database password */
-sed -i -- 's/password_here/'"$PASSWD"'/g' /home/bmlt/public_html/wp-config.php
+sed -i -- 's/password_here/'"$PASSWD"'/g' /home/"$DOMAINUSER"/public_html/wp-config.php
 
 #End WordPress Install
 
@@ -71,22 +62,8 @@ sudo mv wp-cli.phar /usr/local/bin/wp
 #Configure WordPress multisite
 WPADMIN="admin"
 WPADMINPASS="PASSWORD"
-WPSITENAME="BMLT TEST"
-sudo -u $DOMAINUSER wp core multisite-install --path=/home/$DOMAINUSER/public_html/ --url=http://$DOMAIN/ --title="$WPSITENAME" --admin_user=$WPADMIN --admin_password=$WPADMINPASS --admin_email=$DOMAINUSER@$DOMAIN
-sudo -u $DOMAINUSER cp /vagrant/htaccess /home/$DOMAINUSER/public_html/.htaccess
-# installs Desktop Environment
-apt-get -y install x-window-system lxdm leafpad synaptic lxterminal mutt
+WPSITENAME="DO Test"
+sudo -u $DOMAINUSER wp core multisite-install --path=/home/"$DOMAINUSER"/public_html/ --url=http://"$DOMAIN"/ --title="$WPSITENAME" --admin_user=$WPADMIN --admin_password=$WPADMINPASS --admin_email=$DOMAINUSER@$DOMAIN
+wget -cO - https://raw.githubusercontent.com/rdtripp/ubuntu_bmlt/alpha/htaccess >  /home/"$DOMAINUSER"/public_html/.htaccess
 
-#Allows autologin to LXDE as $DOMAINUSER
-sed -i -- 's/# autologin=dgod/autologin='"$DOMAINUSER"'/g' /etc/lxdm/lxdm.conf
-
-#Adds Google Chrome web browser
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-apt-get update && apt-get -y install google-chrome-stable
-
-#Launches Google Chrome on default user login (testing file operations from vagrant)
-cp /usr/share/applications/google-chrome.desktop /etc/xdg/autostart/.
-sed -i -- 's+Exec=/usr/bin/google-chrome-stable %U+Exec=/usr/bin/google-chrome-stable %U  https://'$DOMAIN+'g' /etc/xdg/autostart/google-chrome.desktop
-#Reboots system
 reboot
